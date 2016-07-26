c65452d37fdd368474758ba91f6c41f9c57c3254
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-07-26 10:14:57 +0700

    test auto-deploy

--- emeb-spends.sql
+++ emeb-spends.sql
@@ -92,3 +92,5 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `remember_token`, `creat
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
+/*asdfasf */
